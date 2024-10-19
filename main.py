3d611d30ac16c9c31be6967edc81b83749f2b359
Vishali Thanesh Kumar <118716296+VishaliTk@users.noreply.github.com>
2024-10-19 20:32:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import pandas as pd
 with open('credit_default_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
+with open('scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
+
 
 # Streamlit app interface
 st.title('Credit Card Default Prediction')
