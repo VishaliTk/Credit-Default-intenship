382249f1e0ed569ca5bd2d334337764267b9739d
Vishali Thanesh Kumar <118716296+VishaliTk@users.noreply.github.com>
2024-10-19 19:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 # Create a Streamlit web app
 st.title("Credit Card Default Prediction Dashboard")
+
+# Add input fields for user input
+st.sidebar.header("User Input Features")
